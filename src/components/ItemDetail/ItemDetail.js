06d200ff4cc76620e7f0ba81154c73ae0f8dead1
Lautaro <lautarosantillann@icloud.com>
2021-07-27 17:32:50 -0300

    ItemDetail fixed

--- src/components/ItemDetail/ItemDetail.js
+++ src/components/ItemDetail/ItemDetail.js
@@ -3,40 +3,24 @@ import './ItemDetail.css';
 import Contador from '../Contador/Contador'
 import { useParams } from 'react-router-dom';
 import ItemList from '../../data/productos/itemlist.json'
-function ItemDetail(){
-    const [ productos, setProductos ] = React.useState([])
-    React.useEffect(() => {
-        obtenerDatos()
-        
-    },[])
-    
-        const {id} = useParams()
-        function obtenerDatos() {
-        const Productz = ItemList
-        const Mates = ItemList.MateList
-        const Combos = ItemList.ComboList
-        const Gift = ItemList.GiftBoxList
-        setProductos(Mates)
-
-        const stringed = JSON.stringify(ItemList)
-    }
+import myArr from '../../data/productos/myArr.json'
+function ItemDetail( props ){
+    const {id} = useParams()
+        const Productz = myArr.Products
+        //  const product = myArr.Products.find( x => x.id === id);
+        //  if(!Productz){
+        //     return <h1 stlye={{position: 'relative' , top: '40px'}}> Product {Productz[id].id} Not found </h1>
+        //  }
     return(
         <main>
-        
-            <div style={{position: 'relative', top: '300px'}}>
-            { id === '0' ? <div> hola </div> : <div> chau </div>}
-            
-            </div>
-
         <section className="section">
-            {/* {ItemList.MateList.map(MappedItem =>
-            <div className="grid" key={MappedItem.id === "0"}>
-                   <img className="product__image" src={MappedItem.imagen} alt={"imagen-de-producto" + MappedItem.id}/>
+            <div className="grid">
+            <img className="product__image" src={Productz[id].imagen} alt={`imagen-de-producto ${Productz[id].id}`}/>
             <div className="product__details">
-                <span className="product__tag"> {MappedItem.tipo} </span>
-                <div className="product__name product__title">{MappedItem.nombre}</div>
-                <div className="product__info">{MappedItem.descripcion}</div>
-                <div className="product__price">{'$' + MappedItem.precio}</div>
+                <span className="product__tag"> {Productz[id].tipo} </span>
+                <div className="product__name product__title">{Productz[id].nombre}</div>
+                <div className="product__info"> {Productz[id].descripcion} </div>
+                <div className="product__price">{`$ ${Productz[id].precio}`}</div>
                 <div className="grid--bottom">
                     <div className="contador-container">
                 <Contador/>
@@ -46,11 +30,10 @@ function ItemDetail(){
             <button className="Item-detail-add-to-cart">Añadir al Carrito</button>
                 </div>
                 <div className="slider-image">
-                    <img className="slider-image-img" src={MappedItem.sliderimagen} alt={"imagen-de-producto" + MappedItem.id}></img>
-                    <img className="slider-image-img" src={MappedItem.sliderimagen} alt={"imagen-de-producto" + MappedItem.id}></img>
+                    <img className="slider-image-img" alt={`imagen-de-producto ${Productz[id].id}`}></img>
+                    <img className="slider-image-img"  alt={`imagen-de-producto ${Productz[id].id}`}></img>
                 </div>
             </div>
-                )} */}
         </section>
         </main>
     )}
