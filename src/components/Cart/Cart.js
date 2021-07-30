2f9d97e77850ff897cb0f6729a357e0ad9f52b79
Lautaro <lautarosantillann@icloud.com>
2021-07-30 14:49:03 -0300

    last commit

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -5,7 +5,7 @@ export default function Cart(props) {
   const {products, onAdd} = props; 
   return (
 <main className="block col-2" style={{position: 'relative', top: '150px', width: '100%', height: '100%', background: '#fff'}}>
-<h2>Products</h2>
+<h2>Todos los productos</h2>
 <div className="row">
   {products.map((product) => (
     <Product onAdd={onAdd} key={product.id} product={product}></Product>
